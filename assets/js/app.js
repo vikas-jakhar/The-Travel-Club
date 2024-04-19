d19c39529ec9d2032b3d8a2810bc5affa4f398ab
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-04-19 14:14:31 +0530

    add accordion in section 7

--- assets/js/app.js
+++ assets/js/app.js
@@ -98,12 +98,12 @@ $('.slider').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 2,
                 slidesToScroll: 1,
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
